ca17d36661883256ff9e638487ff2fa56d0165bd
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-06 10:52:39 -0600

    added weather service

--- app/services/base_service.rb
+++ app/services/base_service.rb
@@ -2,4 +2,12 @@ class BaseService
   def self.map_quest
     Faraday.new('http://www.mapquestapi.com')
   end
+
+  def self.open_weather
+    Faraday.new("https://api.openweathermap.org")
+  end
+
+  def self.json_parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
