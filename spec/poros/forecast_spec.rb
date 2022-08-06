9d930b48ec37625623b11a26b24b07989da94b8a
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-06 16:12:47 -0600

    added forecast tests

--- spec/poros/forecast_spec.rb
+++ spec/poros/forecast_spec.rb
@@ -31,11 +31,30 @@ RSpec.describe Forecast do
 
   it "returns a hash with keys when daily weather is called" do
     daily = @forecast.daily(@json[:daily])
-
     expect(daily).to be_a Array
     expect(daily.count).to eq(5)
-    daily = @forecast.first
-    
-    expect(daily[:date])
+
+    one_daily = daily.first
+    expect(one_daily[:datetime]).to eq('2022-08-06')
+    expect(one_daily[:sunrise]).to eq('2022-08-06 06:03:39 -0600')
+    expect(one_daily[:sunset]).to eq('2022-08-06 20:08:02 -0600')
+    expect(one_daily[:max_temp]).to eq(89.89)
+    expect(one_daily[:min_temp]).to eq(73)
+    expect(one_daily[:description]).to eq("light rain")
+    expect(one_daily[:icon]).to eq("10d")
+
+  end
+
+  it "returns a hash with keys when hourly weather is called" do
+    hourly = @forecast.hourly(@json[:hourly])
+
+    expect(hourly).to be_a Array
+    expect(hourly.count).to eq(8)
+
+    one_hour = hourly.first
+    expect(one_hour[:datetime]).to eq('12:00:00')
+    expect(one_hour[:temperature]).to eq(85.33)
+    expect(one_hour[:description]).to eq("scattered clouds")
+    expect(one_hour[:icon]).to eq("03d")
   end
 end
