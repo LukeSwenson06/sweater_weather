e4afa70c478d1a6c258e7ffb0ae70684036c1f55
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-06 16:11:46 -0600

    edited mapquest service

---
+++ spec/services/forecast_service_spec.rb
@@ -0,0 +1,49 @@
+require 'rails_helper'
+
+RSpec.describe "ForecastService" do
+  it "can recieve a call and parse response" do
+    data = File.read('spec/fixtures/forecast.json')
+    response = JSON.parse(data, symbolize_names: true) 
+    expect(response).to be_a Hash
+
+    current = response[:current]
+    expect(response).to include :current, :daily, :hourly
+    expect(current).to include :sunrise, :sunset, :feels_like, :humidity, :weather
+    expect(current[:sunrise]).to be_a(Integer)
+    expect(current[:sunset]).to be_a(Integer)
+    expect(current[:feels_like]).to be_a(Float)
+    expect(current[:humidity]).to be_a(Integer)
+    expect(current[:weather]).to be_a(Array)
+
+    weather = current[:weather].first
+    expect(weather).to include :description, :icon
+    expect(weather[:description]).to be_a(String)
+    expect(weather[:icon]).to be_a(String)
+
+    daily = response[:daily].first
+    expect(daily).to include :dt, :sunrise, :sunset, :temp, :weather
+
+    temp = daily[:temp]
+    expect(temp[:max]).to be_a(Float)
+    expect(temp[:min]).to be_a(Integer)
+
+    daily_weather = daily[:weather].first
+    expect(daily_weather).to include :description, :icon
+    expect(daily_weather[:description]).to be_a(String)
+    expect(daily_weather[:icon]).to be_a(String)
+
+    hourly = response[:hourly]
+    expect(hourly).to be_a(Array)
+
+    hourly_data = hourly.first
+    expect(hourly_data).to include :temp, :weather, :dt
+    expect(hourly_data[:temp]).to be_a(Float)
+    expect(hourly_data[:dt]).to be_a(Integer)
+    expect(hourly_data[:weather]).to be_a(Array)
+
+    hourly_weather = hourly_data[:weather].first
+    expect(hourly_weather).to include :description, :icon
+    expect(hourly_weather[:description]).to be_a(String)
+    expect(hourly_weather[:icon]).to be_a(String)
+  end
+end
---
+++ spec/services/map_quest_service_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe "MapQuestService" do
+  it "can recieve a call and parse response" do
+    state = "Denver,CO"
+
+    response = MapQuestService.get_location_details(state)
+    expect(response).to be_a(Hash)
+
+    parsed_json = response[:results].first[:locations].first[:latLng]
+    expect(parsed_json).to include :lat, :lng
+    expect(parsed_json[:lat]).to eq(39.738453)
+    expect(parsed_json[:lng]).to eq(-104.984853)
+  end
+end
