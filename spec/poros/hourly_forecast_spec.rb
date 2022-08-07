ec9016248e2d67f5df2672008886000f98a8664f
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-07 08:41:50 -0600

    added hourly forecast tests for forecast poro

---
+++ spec/poros/hourly_forecast_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe HourlyForecast do
+  it "populates data with DailyForecast Attributes" do
+    response = File.read('spec/fixtures/forecast.json')
+    json = JSON.parse(response, symbolize_names: true)
+    forecast = HourlyForecast.new(json[:hourly].first)
+  
+    expect(forecast.datetime.first).to eq('12:00:00')
+    expect(forecast.temperature).to eq(85.33)
+    expect(forecast.description).to eq("scattered clouds")
+    expect(forecast.icon).to eq("03d")
+
+  end
+end
