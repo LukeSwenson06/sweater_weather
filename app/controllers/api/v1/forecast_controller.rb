ddcd86dae5e41743075ad09ad936b4f2f6ba6f13
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-07 12:14:54 -0600

    added IF conditional for error handling

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::ForecastController < ApplicationController
   def index
-    mapquest = MapQuestFacade.lat_long(params[:location])
-    forecast = ForecastFacade.weather_details(mapquest.lat, mapquest.lng)
-    render json: ForecastSerializer.new(forecast)
+    if params[:location]
+      mapquest = MapQuestFacade.lat_long(params[:location])
+      forecast = ForecastFacade.weather_details(mapquest.lat, mapquest.lng)
+      render json: ForecastSerializer.new(forecast)
+    else
+      render json: {data: []}, status: 400
+    end
   end
 end
