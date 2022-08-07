cb0f9246b453b269e2a537e8af9ce5fd2a412efb
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-07 08:42:34 -0600

    edited format of spec file

--- spec/poros/daily_forecast_spec.rb
+++ spec/poros/daily_forecast_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe DailyForecast do
     response = File.read('spec/fixtures/forecast.json')
     json = JSON.parse(response, symbolize_names: true)
     forecast = DailyForecast.new(json[:daily].first)
-    expect(forecast.datetime).to eq('2022-08-06')
+
+    expect(forecast.datetime.first).to eq('2022-08-06')
     expect(forecast.sunrise).to eq('2022-08-06 06:03:39 -0600')
     expect(forecast.sunset).to eq('2022-08-06 20:08:02 -0600')
     expect(forecast.max_temp).to eq(89.89)
