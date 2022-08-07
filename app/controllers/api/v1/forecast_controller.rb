8959867e21d00640037e9e324029fe2c2012421f
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-07 08:16:43 -0600

    edited controller

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def index
     mapquest = MapQuestFacade.lat_long(params[:location])
-    forecast = ForecastFacade.weather_details()
+    forecast = ForecastFacade.weather_details(mapquest.lat, mapquest.lng)
   end
 end
