49b65e14aabf43e3240b823ce9e2d4a6c7daf0e1
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-07 10:34:08 -0600

    edited controller to render forecast serializer

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -2,5 +2,6 @@ class Api::V1::ForecastController < ApplicationController
   def index
     mapquest = MapQuestFacade.lat_long(params[:location])
     forecast = ForecastFacade.weather_details(mapquest.lat, mapquest.lng)
+    render json: ForecastSerializer.new(forecast)
   end
 end
