5bb8426f374cec36ca089c853f267bbe67f9a7af
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-07 15:28:15 -0600

    created users controller and added user created method

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+class Api::V1::UsersController < ApplicationController
+  def create
+    user = User.create(user_params)
+    if user.save
+      new_user = user.update(api_key: SecureRandom.hex)
+      render json: UserSerializer.new(user), status: 201
+    elsif params[:password] != params[:password_confirmation]
+      render json: {error: user.errors.full_messages.to_sentence}, status: 400
+    elsif User.find_by(email: user.email)
+      render json: {error: user.errors.full_messages.to_sentence}, status: 400
+    else
+      render json: {error: user.errors.full_messages.to_sentence}, status: 400
+    end
+  end
+
+  private
+  def user_params
+    params.permit(:email, :password, :password_confirmation)
+  end
+end
