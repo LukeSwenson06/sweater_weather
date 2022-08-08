7f5cd69feea825216b3e9d49fbbaf3bb77c66fa1
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-08 15:57:56 -0600

    added service test

--- spec/services/map_quest_service_spec.rb
+++ spec/services/map_quest_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "MapQuestService" do
-  it "can recieve a call and parse response" do
+  it "can recieve a call for location details and parse response" do
     state = "Denver,CO"
 
     response = MapQuestService.get_location_details(state)
@@ -11,5 +11,15 @@ RSpec.describe "MapQuestService" do
     expect(parsed_json).to include :lat, :lng
     expect(parsed_json[:lat]).to eq(39.738453)
     expect(parsed_json[:lng]).to eq(-104.984853)
+  end
+
+  it "can recieve a call for directions details and parse response" do
+    origin = "denver,co"
+    destination = "pueblo,co"
+
+    response = MapQuestService.get_directions_details(origin, destination)
+    expect(response).to be_a(Hash)
+    expect(response[:route]).to include :distance, :formattedTime, :locations
+    expect(response[:route][:locations].first).to include :adminArea5, :adminArea1, :adminArea1
   end
 end
