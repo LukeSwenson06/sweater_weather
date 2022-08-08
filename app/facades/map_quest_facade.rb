3e2719385e2cf9a73c2fd66cc24b86c5e6708599
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-08 15:58:35 -0600

    added service and poro to facade

--- app/facades/map_quest_facade.rb
+++ app/facades/map_quest_facade.rb
@@ -3,4 +3,9 @@ class MapQuestFacade
     json = MapQuestService.get_location_details(state)
     MapQuest.new(json)
   end
+
+  def self.directions_info(origin, destination)
+    json = MapQuestService.get_directions_details(origin, destination)[:route]
+    Directions.new(json)
+  end
 end
