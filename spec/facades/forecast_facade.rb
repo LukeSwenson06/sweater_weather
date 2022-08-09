6913bca1b089fa6c935ede5ff6e1466711b6ef61
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-09 10:19:21 -0600

    Created facade test for future_weather_details and time_convert

--- spec/facades/forecast_facade.rb
+++ spec/facades/forecast_facade.rb
@@ -11,4 +11,21 @@ RSpec.describe "ForecastFacade" do
     expect(forecast[:hourly_weather].count).to eq(8)
 
   end
+
+  it "make a service call and return FutureForecast objects" do
+    response = File.read("spec/fixtures/future_forecast.json")
+    json = JSON.parse(response, symbolize_names: true)
+    forecast = ForecastFacade.future_weather_details(json[:lat], json[:lon],"01:45:23")
+
+    expect(forecast[:conditions]).to eq("clear sky")
+    expect(forecast[:temperature]).to eq(77.16)
+    expect(forecast[:conditions]).to_not eq("few clouds")
+  end
+
+  it "can convert time to single integer" do
+    travel_time = "01:45:23"
+
+    expect(ForecastFacade.time_convert(travel_time)).to eq(1)
+    expect(ForecastFacade.time_convert(travel_time)).to_not eq(travel_time)
+  end
 end
