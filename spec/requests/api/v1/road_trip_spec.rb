1266988ceaa254cae32c65573cafcbbbcdd27317
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-09 14:53:27 -0600

    change formatting

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -86,4 +86,36 @@ describe 'forecast endpoint' do
     expect(attributes[:weather_at_eta]).to be_a Hash
     expect(attributes[:weather_at_eta]).to eq({})
   end
+
+  it "retrieves data " do
+    User.create(email: 'therealemail@email.com', password: "12345", password_confirmation: "12345", api_key: "immanapikey" )
+    params = {
+      "origin": "new york, ny",
+      "destination": "panama",
+      "api_key": "immanapikey"
+    }
+    post '/api/v1/road_trip', params: params
+    expect(response).to have_http_status(200)
+    json =  JSON.parse(response.body, symbolize_names: true)
+    expect(json).to be_a Hash
+    expect(json[:data]).to have_key :id
+    expect(json[:data]).to have_key :type
+    expect(json[:data]).to have_key :attributes
+
+    attributes = json[:data][:attributes]
+    expect(attributes).to have_key :start_city
+    expect(attributes[:start_city]).to be_a String
+    expect(attributes).to have_key :end_city
+    expect(attributes[:end_city]).to be_a String
+    expect(attributes).to have_key :travel_time
+    expect(attributes[:travel_time]).to be_a String
+    expect(attributes).to have_key :weather_at_eta
+    expect(attributes[:weather_at_eta]).to have_key :temperature
+    expect(attributes[:weather_at_eta][:temperature]).to be_a Float
+    expect(attributes[:weather_at_eta]).to have_key :conditions
+    expect(attributes[:weather_at_eta][:conditions]).to be_a String
+
+    expect(attributes[:travel_time]).to_not eq("Impossible Route")
+
+  end
 end
