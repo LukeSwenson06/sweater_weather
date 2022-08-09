aa379f35da3e08f519242ea5d4cb00c1d2c5e316
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-09 10:18:43 -0600

    create road_trip_serializer

---
+++ app/serializers/road_trip_serializer.rb
@@ -0,0 +1,22 @@
+class RoadTripSerializer
+  def self.new(future_forecast, directions)
+
+
+    {
+      "data": {
+        "id": nil,
+        "type": "roadtrip",
+        "attributes": {
+          "start_city": directions.start_city,
+          "end_city": directions.end_city,
+          "travel_time": directions.time,
+          "weather_at_eta":{
+            "temperature": future_forecast[:temperature],
+            "conditions": future_forecast[:conditions]
+
+          }
+        }
+      }
+    }
+  end
+end
