51c8725f1ca85efb4b39228636b90708257146a0
Luke Swenson <LukeSwenson95@outlook.com>
2022-08-09 10:20:14 -0600

    update controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    MapQuestFacade.directions_info(params[:origin], params[:destination])
+    mapquest = MapQuestFacade.lat_long(params[:destination])
+    directions = MapQuestFacade.directions_info(params[:origin], params[:destination])
+    future_forecast = ForecastFacade.future_weather_details(mapquest.lat, mapquest.lng, directions.time)
+    render json: RoadTripSerializer.new(future_forecast, directions)
   end
 end
